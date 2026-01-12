eac77e902dc7011393ef809bba94fee0289c1126
Texper <camfrogtrip@gmail.com>
2026-01-12 14:33:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'texper-v1';
+const urlsToCache = [
+  'index.html',
+  'world-dotted-map.png',
+  'assets/css/main.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
