93022fae573f40af51d1b36c76b9fda9f26d0470
Texper <camfrogtrip@gmail.com>
2026-01-12 14:38:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-const CACHE_NAME = 'texper-v1';
-const urlsToCache = [
-  'index.html',
-  'world-dotted-map.png',
-  'assets/css/main.css'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
   );
 });
